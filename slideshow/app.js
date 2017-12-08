eb770228a5a77c1e7f9409328a5702fbe9d3e719
ey1031 <eyudeveloper@gmail.com>
2017-12-07 20:07:30 -0800

    slideshow: finish

--- slideshow/app.js
+++ slideshow/app.js
@@ -1,28 +1,36 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const state = {
+  FIRST: {'margin-left': '-33%'},
+  SECOND: {'margin-left' : '0%'},
+  THIRD: {'margin-left': '33%'},
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+const scroll = [state.FIRST, state.SECOND, state.THIRD]
+let currentState = -1
+let container = document.getElementsByClassName('container')[0];
+var next = document.getElementsByClassName('nextButton')[0];
+next.addEventListener('click', function() {
+  console.log("next")
+  console.log(currentState)
+  if (currentState != 2) {
+    currentState++;
+    $(".container").css(scroll[currentState]);
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  else {
+    currentState = -1
+    currentState++;
+    $(".container").css(scroll[currentState]);
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+});
+
+var back = document.getElementsByClassName('backButton')[0];
+back.addEventListener('click', function() {
+  console.log("bac")
+  console.log(currentState)
+  if (currentState != -1) {
+    currentState--;
+    $(".container").css(scroll[currentState]);
+  }
+  else {
+    currentState = 2
+    $(".container").css(scroll[currentState]);
+  }
+});
