8d29e4064c2d55be31377aed1ce153fcd08d732c
ey1031 <eyudeveloper@gmail.com>
2018-05-17 13:19:18 -0700

    equipment-system: adding route to change the value of isOut

--- url-shortener/server.js
+++ url-shortener/server.js
@@ -15,6 +15,31 @@ const UrlSchema = mongoose.Schema({
 })
 const Url = mongoose.model('Url', UrlSchema);
 
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('config').secret;
+
+var UserSchema = new mongoose.Schema({
+	username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+	email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},	email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+	bio: String,
+	image: String,
+	hash: String,
+	salt: String
+}, {timestamps: true});
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.methods.setPassword = function(password){
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+UserSchema.methods.validPassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+	return this.hash === hash;
+};
+
+mongoose.model('User', UserSchema);
+
 // EXPRESS
 const express = require('express')
 const hbs = require('hbs')
